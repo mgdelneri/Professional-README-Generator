09ce0b8adb536895ad00d56478da5993fee41178
Marina <mgdelneri@gmail.com>
2021-04-09 11:25:10 -0500

    Included packages needed for the application in index.js; Wrote out inquirer prompt questions.

--- index.js
+++ index.js
@@ -1,4 +1,76 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const markdown = require('./utils/generateMarkdown');
+
+inquirer
+    .prompt([
+        {
+         type: 'input',
+         message: 'What is the title of your project?',
+         name: 'title'
+        },
+        {
+         type: 'input',
+         message: 'What problem does your project solve?/Why build it?',
+         name: 'problem',
+        },
+        {
+         type: 'input',
+         message: 'How did you create this project?',
+         name: 'process',
+        },
+        {
+         type: 'input',
+         message: 'What makes your project stand out?',
+         name: 'identity',
+        },
+        {
+         type: 'input',
+         message: 'What did you learn from working on this project?',
+         name: 'lessons',
+        },
+        {
+         type: 'input',
+         message: 'Please include installation instructions:',
+         name: 'installation',
+        },
+        {
+         type: 'input',
+         message: 'Please include an example of usage:',
+         name: 'usage',
+        },
+        {
+         type: 'input',
+         message: 'Please include a relative filepath to a screenshot of your project:',
+         name: 'screenshot',
+        },
+        {
+         type: 'checkbox',
+         message: 'Please select any relevant licenses:',
+         name: 'license',
+        },
+        {
+         type: 'input',
+         message: 'How can someone contribute to your project?',
+         message: 'contributing',
+        },
+        {
+         type: 'input',
+         message: 'How can someone test your application?',
+         name: 'tests',
+        },
+        {
+         type: 'input',
+         message: 'What is your GitHub username?',
+         name: 'github',
+        },
+        {
+         type: 'input',
+         message: 'What is your email address?',
+         name: 'email',
+        }
+    ])
 
 // TODO: Create an array of questions for user input
 const questions = [];
